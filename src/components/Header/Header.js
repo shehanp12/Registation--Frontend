e7622cc6b882edeb6c1f1c81cba9dce400a68a1b
Shehan Prasanna <shehanp068@gmail.com>
2021-03-20 01:41:03 +0530

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>Registration </header>
+    )
+  }
+}
+
+export default Header;
